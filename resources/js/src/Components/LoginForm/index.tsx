ec74040d3e1998aca68aad7ad53b8d54ae5128dd
guilherme.franzoni <guilherme.sampaio.franzoni@gmail.com>
2023-04-08 04:17:28 -0300

    feat: use form

--- resources/js/src/Components/LoginForm/index.tsx
+++ resources/js/src/Components/LoginForm/index.tsx
@@ -21,6 +21,7 @@ import { FormProvider, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Icon } from "@iconify/react";
+import { Form } from "react-router-dom";
 
 export type LoginFormValues = {
   email: string;
@@ -48,67 +49,69 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit, isLoading }) => {
   })
   return (
     <FormProvider {...form}>
-      <Card boxShadow={'xl'} minWidth={'350px'} maxWidth={'100%'}>
-        <CardHeader>
-          <Text fontSize={'2xl'} fontWeight={'bold'}>Login</Text>
-        </CardHeader>
-        <CardBody py={0}>
-          <VStack alignItems={'start'} spacing={4}>
-            <FormControl isInvalid={!!form.formState.errors.email}>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents="none"
-                  children={<Icon icon={'mdi:email'} width={20} height={20}/>}
-                />
-                <Input id="email" type="email" {...form.register('email')}/>
-              </InputGroup>
-              {form.formState.errors.email && (
-                <FormErrorMessage>{form.formState.errors.email.message}</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl isInvalid={!!form.formState.errors.password}>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents="none"
-                  children={<Icon icon={'mdi:lock'} width={20} height={20}/>}
-                />
-                <Input id="password"
-                       type={form.watch('showPassword') ? 'text' : 'password'} {...form.register('password')}/>
-                <InputRightElement>
-                  <IconButton
-                    variant={'ghost'}
-                    size={'sm'}
-                    aria-label={'Show password'}
-                    icon={<Icon icon={form.watch('showPassword') ? 'mdi:eye-off' : 'mdi:eye'} width={20} height={20}/>}
-                    onClick={() => {
-                      form.setValue('showPassword', !form.watch('showPassword'))
-                    }}
+      <Form onSubmit={form.handleSubmit(onSubmit)}>
+        <Card boxShadow={'xl'} minWidth={'350px'} maxWidth={'100%'}>
+          <CardHeader>
+            <Text fontSize={'2xl'} fontWeight={'bold'}>Login</Text>
+          </CardHeader>
+          <CardBody py={0}>
+            <VStack alignItems={'start'} spacing={4}>
+              <FormControl isInvalid={!!form.formState.errors.email}>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<Icon icon={'mdi:email'} width={20} height={20}/>}
                   />
-                </InputRightElement>
-              </InputGroup>
-              {form.formState.errors.password && (
-                <FormErrorMessage>{form.formState.errors.password.message}</FormErrorMessage>
-              )}
-            </FormControl>
-          </VStack>
-        </CardBody>
-        <CardFooter>
-          <HStack justifyContent={'flex-end'} width={'100%'}>
-            <Button
-              size={'sm'}
-              colorScheme={'whatsapp'}
-              type={'submit'}
-              onClick={form.handleSubmit(onSubmit)}
-              variant={'solid'}
-              isLoading={isLoading}
-            >
-              Login
-            </Button>
-          </HStack>
-        </CardFooter>
-      </Card>
+                  <Input id="email" type="email" {...form.register('email')}/>
+                </InputGroup>
+                {form.formState.errors.email && (
+                  <FormErrorMessage>{form.formState.errors.email.message}</FormErrorMessage>
+                )}
+              </FormControl>
+              <FormControl isInvalid={!!form.formState.errors.password}>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<Icon icon={'mdi:lock'} width={20} height={20}/>}
+                  />
+                  <Input id="password"
+                         type={form.watch('showPassword') ? 'text' : 'password'} {...form.register('password')}/>
+                  <InputRightElement>
+                    <IconButton
+                      variant={'ghost'}
+                      size={'sm'}
+                      aria-label={'Show password'}
+                      icon={<Icon icon={form.watch('showPassword') ? 'mdi:eye-off' : 'mdi:eye'} width={20}
+                                  height={20}/>}
+                      onClick={() => {
+                        form.setValue('showPassword', !form.watch('showPassword'))
+                      }}
+                    />
+                  </InputRightElement>
+                </InputGroup>
+                {form.formState.errors.password && (
+                  <FormErrorMessage>{form.formState.errors.password.message}</FormErrorMessage>
+                )}
+              </FormControl>
+            </VStack>
+          </CardBody>
+          <CardFooter>
+            <HStack justifyContent={'flex-end'} width={'100%'}>
+              <Button
+                size={'sm'}
+                colorScheme={'whatsapp'}
+                type={'submit'}
+                variant={'solid'}
+                isLoading={isLoading}
+              >
+                Login
+              </Button>
+            </HStack>
+          </CardFooter>
+        </Card>
+      </Form>
     </FormProvider>
   )
 }
